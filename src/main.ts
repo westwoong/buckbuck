eeafcee14020f304570432e43829d20de7ac01b2
westwoong <westwoong>
2023-09-09 08:09:47 +0900

    Feat : TypeORM Transactional 설정 적용

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import * as dotenv from 'dotenv';
 import {ValidationPipe} from "@nestjs/common";
+import {initializeTransactionalContext} from "typeorm-transactional";
 
 async function bootstrap() {
+    initializeTransactionalContext();
     dotenv.config();
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({
