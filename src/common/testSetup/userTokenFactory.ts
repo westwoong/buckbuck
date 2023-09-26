d6da491e9225b9ab290cbf08daf1a93efd4ae3b2
westwoong <westwoong>
2023-09-26 11:24:01 +0900

    Feat : testSetup user Fixture 설정

---
+++ src/common/testSetup/userTokenFactory.ts
@@ -0,0 +1,26 @@
+import {DataSource} from "typeorm";
+import {UserEntity} from "../../users/User.entity";
+import {AuthService} from "../../auth/auth.service";
+
+export class UserTokenFactory {
+    private dataSource: DataSource;
+    private authService: AuthService;
+
+    constructor(dataSource: DataSource, authService: AuthService) {
+        this.dataSource = dataSource;
+        this.authService = authService;
+    }
+
+    public async createUserToken() {
+        const user = await this.dataSource.getRepository(UserEntity).save({
+            account: "xptmxmlqslek123",
+            password: "testpassword123",
+            name: '홍길동',
+            email: "test11r@example.com",
+            phoneNumber: "01052828282",
+            nickName: "빨리점11",
+        });
+        return this.authService.signInWithJwt({ userId: user.id })
+
+    }
+}
--- src/posts/test/post.e2e.spec.ts
+++ src/posts/test/post.e2e.spec.ts
@@ -5,11 +5,13 @@ import {initializeTransactionalContext} from 'typeorm-transactional';
 import * as dotenv from 'dotenv';
 import * as request from 'supertest';
 import {DataSource} from "typeorm";
+import {AuthService} from "../../auth/auth.service";
+import {UserTokenFactory} from '../../common/testSetup/userTokenFactory'
 
 describe('PostController (E2E)', () => {
     let app: INestApplication;
     let dataSource: DataSource;
-    let userToken: string;
+    let authService: AuthService;
 
     beforeAll(async () => {
         initializeTransactionalContext();
@@ -19,6 +21,7 @@ describe('PostController (E2E)', () => {
         }).compile();
 
         dataSource = moduleRef.get<DataSource>(DataSource);
+        authService = moduleRef.get<AuthService>(AuthService);
         app = moduleRef.createNestApplication();
         app.useGlobalPipes(new ValidationPipe({transform: true}));
         await app.init();
@@ -27,23 +30,15 @@ describe('PostController (E2E)', () => {
     beforeEach(async () => {
         await dataSource.dropDatabase();
         await dataSource.synchronize();
-        await request(app.getHttpServer()).post('/users/signup').send({
-            account: "xptmxmlqslek123",
-            password: "testpassword123",
-            name: "김돌쇠",
-            email: "test11r@example.com",
-            phoneNumber: "01052828282",
-            nickName: "빨리점11"
-        })
-        let userLogin = await request(app.getHttpServer()).post('/users/signin').send({
-            account: "xptmxmlqslek123",
-            password: "testpassword123",
-        })
-        userToken = userLogin.text;
     })
 
     describe('create', () => {
         it('게시글을 작성하면 201로 응답한다.', async () => {
+            // Given = 테스트 사전 Fixture
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            // When =
             const response = await request(app.getHttpServer())
                 .post('/posts')
                 .send({
@@ -53,48 +48,8 @@ describe('PostController (E2E)', () => {
                     level: '고수'
                 }).set('Authorization', `Bearer ${userToken}`);
 
+            // then
             expect(response.status).toBe(201);
-        })
-
-        it('게시글 작성 중 하나라도 넣지않으면 400 코드를 반환한다.', async () => {
-            await request(app.getHttpServer())
-                .post('/posts')
-                .send({
-                    title: '테스트 제목입니다.',
-                }).set('Authorization', `Bearer ${userToken}`)
-                .expect(400);
-
-            await request(app.getHttpServer())
-                .post('/posts')
-                .send({
-                    content: '테스트 내용입니다.',
-                }).set('Authorization', `Bearer ${userToken}`)
-                .expect(400);
-
-            await request(app.getHttpServer())
-                .post('/posts')
-                .send({
-                    cost: 10500,
-                }).set('Authorization', `Bearer ${userToken}`)
-                .expect(400);
-
-            await request(app.getHttpServer())
-                .post('/posts')
-                .send({
-                    level: '고수'
-                }).set('Authorization', `Bearer ${userToken}`)
-                .expect(400);
-        })
-
-        it('로그인을 안했을 시 401 코드를 반환한다', async () => {
-            await request(app.getHttpServer())
-                .post('/posts')
-                .send({
-                    title: '테스트 제목입니다.',
-                    content: '테스트 내용입니다.',
-                    cost: 10500,
-                    level: '고수'
-                }).expect(401);
         })
 
     })
