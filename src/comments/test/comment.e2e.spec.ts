51555d2be796acfad709efcd85321f7717e51312
westwoong <westwoong>
2023-10-02 18:08:20 +0900

    Feat : 댓글 작성, 수정 테스트 코드 작성

--- src/comments/test/comment.e2e.spec.ts
+++ src/comments/test/comment.e2e.spec.ts
@@ -8,8 +8,9 @@ import {DataSource} from "typeorm";
 import {AuthService} from "../../auth/auth.service";
 import {UserTokenFactory} from '../../common/testSetup/userTokenFactory'
 import {PostFactory} from "../../common/testSetup/postFactory";
+import {CommentFactory} from "../../common/testSetup/commentFactory";
 
-describe('PostController (E2E)', () => {
+describe('CommentController (E2E)', () => {
     let app: INestApplication;
     let dataSource: DataSource;
     let authService: AuthService;
@@ -33,80 +34,57 @@ describe('PostController (E2E)', () => {
         await dataSource.synchronize();
     })
 
-    describe('create Post', () => {
-        it('게시글을 작성 시 body값이 fixture 값과 동일해야한다.', async () => {
-            // Given = 테스트 사전 Fixture
+    describe('create Comment', () => {
+        it('댓글 작성 시 httpcode 201로 응답한다. ', async () => {
             const userTokenFactory = new UserTokenFactory(dataSource, authService);
             const userToken = await userTokenFactory.createUserToken();
+            const userId = await userTokenFactory.userId();
+            const postFactory = new PostFactory(dataSource, userId);
+            const post = await postFactory.createPost();
 
-            const fixturePost = {
-                title: '테스트 제목입니다.',
-                content: '테스트 내용입니다.',
-                cost: 10500,
-                level: '고수'
+            const fixtureComment = {
+                content: '테스트 댓글 달아봅니다.',
+                proposalCost: 15000
             }
-            // When =
+
             const response = await request(app.getHttpServer())
-                .post('/posts')
-                .send(fixturePost)
+                .post(`/comments/${post.id}`)
+                .send(fixtureComment)
                 .set('Authorization', `Bearer ${userToken}`);
-            // then
+
             expect(response.status).toBe(201);
-            expect(response.body.title).toBe(fixturePost.title);
-            expect(response.body.content).toBe(fixturePost.content);
-            expect(response.body.cost).toBe(fixturePost.cost);
-            expect(response.body.level).toBe(fixturePost.level);
+            expect(response.body.content).toBe(fixtureComment.content);
+            expect(response.body.proposalCost).toBe(fixtureComment.proposalCost);
         })
     })
 
-    describe('modify Post', () => {
-        it('게시글 수정 시 httpcode 200으로 응답한다', async () => {
+    describe('modify Comment', () => {
+        it('댓글 수정 시 httpcode 200으로 응답한다.', async () => {
             const userTokenFactory = new UserTokenFactory(dataSource, authService);
             const userToken = await userTokenFactory.createUserToken();
             const userId = await userTokenFactory.userId();
             const postFactory = new PostFactory(dataSource, userId);
             const post = await postFactory.createPost();
-            const postId = post.id;
+            const commentFactory = new CommentFactory(dataSource, userId, post.id);
+            const comment = await commentFactory.createComment();
 
-            const modifyPost = {
-                title: '수정 테스트 입니다..',
-                content: '내용도 수정해볼게요',
-                cost: 50000,
-                level: '초급'
+            const modifyComment = {
+                content: '테스트 댓글 달아봅니다.',
+                proposalCost: 15000
             }
 
             const response = await request(app.getHttpServer())
-                .patch(`/posts/${postId}`)
-                .send(modifyPost)
+                .patch(`/comments/${comment.id}`)
+                .send(modifyComment)
                 .set('Authorization', `Bearer ${userToken}`);
 
-            const fixedPost = await postFactory.getPost();
-            console.log(fixedPost);
+            const fixedComment = await commentFactory.getComment();
+            console.log(fixedComment)
             expect(response.status).toBe(200);
-        })
-    })
-
-    describe('delete Post', () => {
-        it('게시글 삭제 시 httpcode 204로 응답한다.', async () => {
-            const userTokenFactory = new UserTokenFactory(dataSource, authService);
-            const userToken = await userTokenFactory.createUserToken();
-            const userId = await userTokenFactory.userId();
-            const postFactory = new PostFactory(dataSource, userId);
-            const post = await postFactory.createPost();
-            const postId = post.id;
-
-            const response = await request(app.getHttpServer())
-                .delete(`/posts/${postId}`)
-                .set('Authorization', `Bearer ${userToken}`);
-
-            const isExistPost = await postFactory.getPost();
-            console.log(isExistPost);
-            expect(response.status).toBe(204);
-            expect(isExistPost).toHaveLength(0);
         })
     })
 
     afterAll(async () => {
         await app.close();
-    });
+    })
 })
