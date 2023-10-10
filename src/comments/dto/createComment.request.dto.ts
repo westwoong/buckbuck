4e2644d45fb56ee5c545d43cd56f08ae4dd53b3c
westwoong <westwoong>
2023-10-10 13:33:49 +0900

    Feat : 댓글 작성 유효성검증 옵션 추가

--- src/comments/dto/createComment.request.dto.ts
+++ src/comments/dto/createComment.request.dto.ts
@@ -1,9 +1,14 @@
-import {IsNotEmpty} from "class-validator";
+import {IsInt, IsNotEmpty, IsString, Length, Max, Min} from "class-validator";
 
 export class CreateCommentRequestDto {
     @IsNotEmpty()
+    @IsString()
+    @Length(1, 30)
     content: string;
 
     @IsNotEmpty()
+    @IsInt()
+    @Min(0)
+    @Max(100000)
     proposalCost: number;
 }
