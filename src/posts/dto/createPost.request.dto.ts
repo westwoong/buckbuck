c5ccdbd82910929b4604b264b6a8fb2ca1e105c6
westwoong <westwoong>
2023-10-10 11:44:12 +0900

    Feat : createPostRequestDto 유효성 검증 옵션 추가

--- src/posts/dto/createPost.request.dto.ts
+++ src/posts/dto/createPost.request.dto.ts
@@ -1,15 +1,21 @@
-import {IsNotEmpty} from "class-validator";
+import {IsInt, IsNotEmpty, IsString, Length, Min} from "class-validator";
 
 export class CreatePostRequestDto {
     @IsNotEmpty()
+    @IsString()
+    @Length(1,30)
     title: string;
 
     @IsNotEmpty()
+    @IsString()
     content: string;
 
     @IsNotEmpty()
+    @IsInt()
+    @Min(0)
     cost: number;
 
     @IsNotEmpty()
+    @IsString()
     level: string;
 }
