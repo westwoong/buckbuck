cc4234bf29dc34be8891b6088440f76421115f54
westwoong <westwoong>
2023-10-10 11:22:34 +0900

    Feat : createPostRequestDto 테스트 코드 분리

---
+++ src/posts/test/post.requestDto.spec.ts
@@ -0,0 +1,72 @@
+import {INestApplication, ValidationPipe} from '@nestjs/common';
+import {Test, TestingModule} from '@nestjs/testing';
+import {AppModule} from '../../app.module';
+import {initializeTransactionalContext} from 'typeorm-transactional';
+import * as dotenv from 'dotenv';
+import * as request from 'supertest';
+import {DataSource} from "typeorm";
+import {AuthService} from "../../auth/auth.service";
+import {UserTokenFactory} from '../../common/testSetup/userTokenFactory'
+import {PostFactory} from "../../common/testSetup/postFactory";
+import {CreatePostRequestDto} from "../dto/createPost.request.dto";
+import {validate} from "class-validator";
+
+
+describe('createPost RequestDto', () => {
+    describe('게시글 작성 API', () => {
+        describe('title 유효성 검사', () => {
+            const createPostRequestDto = new CreatePostRequestDto();
+            it.each([
+                ['테스트 제목입니다', true],
+                ['', false]
+            ])('title 필드 유효성 검사에 이상이 없을 시 error의 길이가 0이여야한다.', async (title, isValid) => {
+                createPostRequestDto.title = title;
+                const errors = await validate(createPostRequestDto, {skipMissingProperties: true});
+
+                if (isValid) expect(errors).toHaveLength(0);
+                if (!isValid) expect(errors).not.toHaveLength(0);
+            })
+        })
+
+        describe('content 유효성 검사', () => {
+            const createPostRequestDto = new CreatePostRequestDto();
+            it.each([
+                ['테스트 본문 내용입니다', true],
+                ['', false]
+            ])('content 필드 유효성 검사에 이상이 없을 시 error의 길이가 0이여야한다.', async (content, isValid) => {
+                createPostRequestDto.content = content;
+                const errors = await validate(createPostRequestDto, {skipMissingProperties: true});
+
+                if (isValid) expect(errors).toHaveLength(0);
+                if (!isValid) expect(errors).not.toHaveLength(0);
+            })
+        })
+
+        describe('cost 유효성 검사', () => {
+            const createPostRequestDto = new CreatePostRequestDto();
+            it.each([
+                [15000, true], // 정상
+            ])('cost 필드 유효성 검사에 이상이 없을 시 error의 길이가 0이여야한다.', async (cost, isValid) => {
+                createPostRequestDto.cost = cost;
+                const errors = await validate(createPostRequestDto, {skipMissingProperties: true});
+
+                if (isValid) expect(errors).toHaveLength(0);
+                if (!isValid) expect(errors).not.toHaveLength(0);
+            })
+        })
+
+        describe('level 유효성 검사', () => {
+            const createPostRequestDto = new CreatePostRequestDto();
+            it.each([
+                ['고수', true],
+                ['', false]
+            ])('level 필드 유효성 검사에 이상이 없을 시 error의 길이가 0이여야한다.', async (level, isValid) => {
+                createPostRequestDto.level = level;
+                const errors = await validate(createPostRequestDto, {skipMissingProperties: true});
+
+                if (isValid) expect(errors).toHaveLength(0);
+                if (!isValid) expect(errors).not.toHaveLength(0);
+            })
+        })
+    })
+})
