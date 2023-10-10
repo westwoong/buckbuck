8d72d0d3be2b8e64574ed8849d57670ce8fa403b
westwoong <westwoong>
2023-10-10 15:15:27 +0900

    Refactor : 회원가입 e2e 유효성 검증 케이스 분리

--- src/users/test/user.e2e.spec.ts
+++ src/users/test/user.e2e.spec.ts
@@ -5,8 +5,6 @@ import * as request from 'supertest';
 import * as dotenv from 'dotenv';
 import {initializeTransactionalContext} from "typeorm-transactional";
 import {DataSource} from "typeorm";
-import {SignUpRequestDto} from "../dto/signUp.request.dto";
-import {validate} from "class-validator";
 
 describe('UserController (E2E)', () => {
     let app: INestApplication;
@@ -30,110 +28,6 @@ describe('UserController (E2E)', () => {
         await dataSource.dropDatabase();
         await dataSource.synchronize();
     })
-
-    describe('회원가입 account 유효성 검사', () => {
-        const signUpDto = new SignUpRequestDto();
-        it.each([
-            ['xptmxmlqslek123', true], // 검사 통과 정상 아이디 값
-            ['', false], // 아이디 미입력 검사
-            ['XPTMXMDLQSLEK123', false],       // 대문자 검사
-            ['xptmxmdlqslek!@#', false],       // 특수문자 검사
-            ['tt1', false], // 길이 미달 검사
-            ['xptmxmdxpmxpxmpxmpxmpxmlqslek!@#', false], // 길이 초과 검사
-
-        ])('account 필드 유효성 검사에 이상이 없을 시 errors의 길이가 0이여야한다.', async (account, isValid) => {
-            signUpDto.account = account;
-            const errors = await validate(signUpDto, {skipMissingProperties: true});
-
-            if (isValid) expect(errors).toHaveLength(0);
-            if (!isValid) expect(errors).not.toHaveLength(0);
-        });
-    });
-
-    describe('회원가입 password 유효성 검사', () => {
-        const signUpDto = new SignUpRequestDto();
-        it.each([
-            ['testpassword123', true], // 정상 패스워드값
-            ['test password 123', false], // 비밀번호 공백 포함
-            ['pw1234', false], // 비밀번호 길이 미달
-            ['thisPasswordLengthToLong!#@12312', false], // 비밀번호 길이 초과
-        ])('password 필드 유효성 검사에 이상이 없을 시 errors의 길이가 0이여야한다.', async (password, isValid) => {
-            signUpDto.password = password;
-            const errors = await validate(signUpDto, {skipMissingProperties: true});
-
-            if (isValid) expect(errors).toHaveLength(0);
-            if (!isValid) expect(errors).not.toHaveLength(0);
-        });
-    });
-
-    describe('회원가입 name 유효성 검사', () => {
-        const signUpDto = new SignUpRequestDto();
-        it.each([
-            ['김돌쇠', true], // 정상 이름
-            ['', false], // 이름이 공백
-            ['jonson', false], // 영문 이름
-            ['웅', false], // 이름 길이 미달
-            ['김수한무거북이와두루미삼천갑자', false], // 이름 길이 초과
-        ])('name 필드 유효성 검사에 이상이 없을 시 errors의 길이가 0이여야한다.', async (name, isValid) => {
-            signUpDto.name = name;
-            const errors = await validate(signUpDto, {skipMissingProperties: true});
-
-            if (isValid) expect(errors).toHaveLength(0);
-            if (!isValid) expect(errors).not.toHaveLength(0);
-        });
-    });
-
-    describe('회원가입 email 유효성 검사', () => {
-        const signUpDto = new SignUpRequestDto();
-        it.each([
-            ['test11r@example.com', true], // 정상 이메일
-            ['test 11r@example.com', false], // 공백 포함 시
-            ['test11rexample.com', false], // @ 미포함 시
-            ['test11r@.com', false], // 도메인 미포함 시
-            ['test11r@example', false], // 최상위 도메인 미포함 시
-        ])('email 필드 유효성 검사에 이상이 없을 시 errors의 길이가 0이여야한다.', async (email, isValid) => {
-            signUpDto.email = email;
-            const errors = await validate(signUpDto, {skipMissingProperties: true});
-
-            if (isValid) expect(errors).toHaveLength(0);
-            if (!isValid) expect(errors).not.toHaveLength(0);
-        });
-    });
-
-    describe('회원가입 phoneNumber 유효성 검사', () => {
-        const signUpDto = new SignUpRequestDto();
-        it.each([
-            ['01052828282', true], // 정상적인 번호
-            ['010-5282-8282', false], // 하이픈 포함
-            ['52828282', false], // 010 미포함(길이미달)
-            ['010 5282 8282', false], // 공백 포함 시
-            ['010528282821', false], // 길이 초과 시
-            ['공일공52828282', false], // 숫자 외 문자 포함 시
-        ])('phoneNumber 필드 유효성 검사에 이상이 없을 시 errors의 길이가 0이여야한다.', async (phoneNumber, isValid) => {
-            signUpDto.phoneNumber = phoneNumber;
-            const errors = await validate(signUpDto, {skipMissingProperties: true});
-
-            if (isValid) expect(errors).toHaveLength(0);
-            if (!isValid) expect(errors).not.toHaveLength(0);
-        });
-    });
-
-    describe('회원가입 nickName 유효성 검사', () => {
-        const signUpDto = new SignUpRequestDto();
-        it.each([
-            ['빨리점11', true], // 정상적인 닉네임
-            ['', false], // 공백인 경우
-            ['_빨리점_', false], // 특수문자가 포함된 경우
-            ['왕', false], // 길이 미달
-            ['빨리빨리빨리좀제발빨리좀', false], // 길이 초과
-        ])('phoneNumber 필드 유효성 검사에 이상이 없을 시 errors의 길이가 0이여야한다.', async (nickName, isValid) => {
-            signUpDto.nickName = nickName;
-            const errors = await validate(signUpDto, {skipMissingProperties: true});
-
-            if (isValid) expect(errors).toHaveLength(0);
-            if (!isValid) expect(errors).not.toHaveLength(0);
-        });
-    });
 
     describe('/users/signup (POST)', () => {
         it('회원가입에 성공하면 201로 응답한다.', async () => {
