d0a2c73040f4c94774361da5a3a85a58e037a403
westwoong <westwoong>
2023-10-10 15:24:27 +0900

    Feat : 회원가입 e2e 필드값 유무 테스트 케이스 추가

--- src/users/test/user.e2e.spec.ts
+++ src/users/test/user.e2e.spec.ts
@@ -30,16 +30,84 @@ describe('UserController (E2E)', () => {
     })
 
     describe('/users/signup (POST)', () => {
-        it('회원가입에 성공하면 201로 응답한다.', async () => {
-            const response = await request(app.getHttpServer()).post('/users/signup').send({
-                account: "xptmxmlqslek123",
-                password: "testpassword123",
-                name: "김돌쇠",
-                email: "test11r@example.com",
-                phoneNumber: "01052828282",
-                nickName: "빨리점11"
+        describe('회원가입 시 httpcode 응답 값이 정상인지 확인한다.', () => {
+            it('회원가입에 성공하면 201로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    account: "xptmxmlqslek123",
+                    password: "testpassword123",
+                    name: "김돌쇠",
+                    email: "test11r@example.com",
+                    phoneNumber: "01052828282",
+                    nickName: "빨리점11"
+                })
+                expect(response.status).toBe(201);
             })
-            expect(response.status).toBe(201);
+
+            it('account의 값이 비어있을 시 400으로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    password: "testpassword123",
+                    name: "김돌쇠",
+                    email: "test11r@example.com",
+                    phoneNumber: "01052828282",
+                    nickName: "빨리점11"
+                })
+                expect(response.status).toBe(400);
+            })
+
+            it('password의 값이 비어있을 시 400으로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    account: "xptmxmlqslek123",
+                    name: "김돌쇠",
+                    email: "test11r@example.com",
+                    phoneNumber: "01052828282",
+                    nickName: "빨리점11"
+                })
+                expect(response.status).toBe(400);
+            })
+
+            it('name의 값이 비어있을 시 400으로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    account: "xptmxmlqslek123",
+                    password: "testpassword123",
+                    email: "test11r@example.com",
+                    phoneNumber: "01052828282",
+                    nickName: "빨리점11"
+                })
+                expect(response.status).toBe(400);
+            })
+
+            it('email의 값이 비어있을 시 400으로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    account: "xptmxmlqslek123",
+                    password: "testpassword123",
+                    name: "김돌쇠",
+                    phoneNumber: "01052828282",
+                    nickName: "빨리점11"
+                })
+                expect(response.status).toBe(400);
+            })
+
+            it('phoneNumber의 값이 비어있을 시 400으로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    account: "xptmxmlqslek123",
+                    password: "testpassword123",
+                    name: "김돌쇠",
+                    email: "test11r@example.com",
+                    nickName: "빨리점11"
+                })
+                expect(response.status).toBe(400);
+            })
+
+            it('nickName의 값이 비어있을 시 400으로 응답한다.', async () => {
+                const response = await request(app.getHttpServer()).post('/users/signup').send({
+                    account: "xptmxmlqslek123",
+                    password: "testpassword123",
+                    name: "김돌쇠",
+                    email: "test11r@example.com",
+                    phoneNumber: "01052828282",
+                })
+                expect(response.status).toBe(400);
+            })
         })
     })
 
