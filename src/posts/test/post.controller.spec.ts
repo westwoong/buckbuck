a11f5157f90959b11a1d1ff6c3cc4c82633d56fd
westwoong <westwoong>
2023-10-21 01:31:09 +0900

    Test : post patch e2e 유효성검증 케이스 controller layer 테스트로 변경

--- src/posts/test/post.controller.spec.ts
+++ src/posts/test/post.controller.spec.ts
@@ -29,7 +29,7 @@ describe('PostController', () => {
         await app.init();
     });
     describe('/posts (POST)', () => {
-        it('httpcode 201로 응답한다', async () => {
+        it('정상적인 요청시 201 응답코드를 반환한다.', async () => {
             const userTokenFactory = new UserTokenFactory(dataSource, authService);
             const userToken = await userTokenFactory.createUserToken();
             return request(app.getHttpServer())
@@ -94,14 +94,15 @@ describe('PostController', () => {
                 .send({
                     title: '테스트 제목입니다.',
                     content: '테스트 내용입니다.',
-                    cost: 10500
+                    cost: 10500,
+                    level: '고수'
                 })
                 .expect(401);
         })
     })
 
     describe('/posts/:postId (PATCH)', () => {
-        it('httpcode 200으로 응답한다.', async () => {
+        it('정상적인 요청 시 200 응답코드를 반환한다.', async () => {
             const userTokenFactory = new UserTokenFactory(dataSource, authService);
             const userToken = await userTokenFactory.createUserToken();
             return request(app.getHttpServer())
@@ -114,7 +115,65 @@ describe('PostController', () => {
                 }).set('Authorization', `Bearer ${userToken}`)
                 .expect(200)
         })
+
+        it('title 의 값이 비어있을 시 400 코드로 응답한다', async () => {
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            return request(app.getHttpServer())
+                .patch(`/posts/1`)
+                .send({
+                    content: '내용도 수정해볼게요',
+                    cost: 50000,
+                    level: '초급'
+                })
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(400);
+        })
+
+        it('content 의 값이 비어있을 시 400 코드로 응답한다', async () => {
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            return request(app.getHttpServer())
+                .patch('/posts/1')
+                .send({
+                    title: '테스트 제목입니다.',
+                    cost: 50000,
+                    level: '초급'
+                })
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(400);
+        })
+
+        it('level 의 값이 비어있을 시 400 코드로 응답한다', async () => {
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            return request(app.getHttpServer())
+                .patch('/posts/1')
+                .send({
+                    title: '수정 테스트 입니다..',
+                    content: '내용도 수정해볼게요',
+                    cost: 50000,
+                })
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(400);
+        })
+
+        it('userToken 이 없을 시 401 코드로 응답한다', async () => {
+            return request(app.getHttpServer())
+                .patch('/posts/1')
+                .send({
+                    title: '수정 테스트 입니다..',
+                    content: '내용도 수정해볼게요',
+                    cost: 50000,
+                    level: '초급'
+                })
+                .expect(401);
+        })
     })
+
     describe('/posts/:postId (DELETE)', () => {
         it('httpcode 204로 응답한다.', async () => {
             const userTokenFactory = new UserTokenFactory(dataSource, authService);
