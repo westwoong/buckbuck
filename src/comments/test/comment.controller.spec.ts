e023449481dd88b28a1c6c356a0c30f104f3af3c
westwoong <westwoong>
2023-10-21 02:15:20 +0900

    Test : comment PATCH e2e 유효성검증 케이스 controller 테스트로 분리

--- src/comments/test/comment.controller.spec.ts
+++ src/comments/test/comment.controller.spec.ts
@@ -7,6 +7,9 @@ import * as request from "supertest";
 import {UserTokenFactory} from "../../common/testSetup/user/userTokenFactory";
 import {DataSource} from "typeorm";
 import {AuthService} from "../../auth/auth.service";
+import {UserFinder} from "../../common/testSetup/user/userFinder";
+import {PostFactory} from "../../common/testSetup/post/postFactory";
+import {CommentFactory} from "../../common/testSetup/comment/commentFactory";
 
 jest.mock('../comment.service');
 
@@ -67,6 +70,54 @@ describe('CommentController', () => {
         it('userToken 이 없을 시 401 코드로 응답한다.', async () => {
             return request(app.getHttpServer())
                 .post(`/comments/:postId`)
+                .send({
+                    content: '테스트 댓글 달아봅니다.',
+                    proposalCost: 15000
+                })
+                .expect(401);
+        })
+    })
+
+    describe('/comments/:commentId (PATCH)', () => {
+        it('정상적인 요청 시 200 응답코드를 반환한다.', async () => {
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            return request(app.getHttpServer())
+                .patch(`/comments/:commentId`)
+                .send({
+                    content: '테스트 댓글 수정해봅니다.',
+                    proposalCost: 50500
+                })
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(200);
+        })
+
+        it('content 의 값이 비어있을 시 400으로 응답한다.', async () => {
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            return request(app.getHttpServer())
+                .patch(`/comments/:commentId`)
+                .send({proposalCost: 50500})
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(400);
+        })
+
+        it('proposalCost 의 값이 비어있을 시 400으로 응답한다.', async () => {
+            const userTokenFactory = new UserTokenFactory(dataSource, authService);
+            const userToken = await userTokenFactory.createUserToken();
+
+            return request(app.getHttpServer())
+                .patch(`/comments/:commentId`)
+                .send({content: '테스트 댓글 수정해봅니다.'})
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(400)
+        })
+
+        it('userToken 이 없을 시 401 코드로 응답한다.', async () => {
+            return request(app.getHttpServer())
+                .patch(`/comments/:commentId`)
                 .send({
                     content: '테스트 댓글 달아봅니다.',
                     proposalCost: 15000
