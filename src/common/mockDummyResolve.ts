91f62bba35f97b155c87964a296d9c869698f436
westwoong <westwoong>
2023-11-04 18:34:51 +0900

    Test : 모킹용 resolve 설정파일 추가

---
+++ src/common/mockDummyResolve.ts
@@ -0,0 +1,10 @@
+import {UserEntity} from "../users/User.entity";
+
+export const DUMMY_USER_RESOLVE = new UserEntity({
+    account: "xptmxmlqslek123",
+    password: "testpassword123",
+    name: "홍길동",
+    email: "test11r@example.com",
+    phoneNumber: "01052828282",
+    nickName: "빨리점11"
+})
