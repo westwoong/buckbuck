acc6a7dad0f48a0731984252372b922023d76889
westwoong <westwoong>
2023-11-05 00:52:59 +0900

    Test : spyOn 모킹용 더미 comment, review Resolve 추가

--- src/common/mockDummyResolve.ts
+++ src/common/mockDummyResolve.ts
@@ -1,5 +1,7 @@
 import {UserEntity} from "../users/User.entity";
 import {PostEntity} from "../posts/Post.entity";
+import {CommentEntity} from "../comments/Comment.entity";
+import {ReviewEntity} from "../reviews/Review.entity";
 
 export const DUMMY_USER_RESOLVE = new UserEntity({
     account: "xptmxmlqslek123",
@@ -15,4 +17,15 @@ export const DUMMY_POST_RESOLVE = new PostEntity({
     content: '테스트 내용입니다.',
     cost: 10500,
     level: '고수'
+})
+
+export const DUMMY_COMMENT_RESOLVE = new CommentEntity({
+    content: '테스트 댓글 달아봅니다.',
+    proposalCost: 15000
+})
+
+export const DUMMY_REVIEW_RESOLVE = new ReviewEntity({
+    post: DUMMY_POST_RESOLVE,
+    stars: 5,
+    comment: '친절해요'
 })
