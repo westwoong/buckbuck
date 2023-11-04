8e35922e1c50a66f60686378e973b5ac9c7aaac5
westwoong <westwoong>
2023-11-04 19:06:31 +0900

    Test : mocking용 dummy_post_resolve 추가

--- src/common/mockDummyResolve.ts
+++ src/common/mockDummyResolve.ts
@@ -1,4 +1,5 @@
 import {UserEntity} from "../users/User.entity";
+import {PostEntity} from "../posts/Post.entity";
 
 export const DUMMY_USER_RESOLVE = new UserEntity({
     account: "xptmxmlqslek123",
@@ -7,4 +8,11 @@ export const DUMMY_USER_RESOLVE = new UserEntity({
     email: "test11r@example.com",
     phoneNumber: "01052828282",
     nickName: "빨리점11"
+})
+
+export const DUMMY_POST_RESOLVE = new PostEntity({
+    title: '테스트 제목입니다.',
+    content: '테스트 내용입니다.',
+    cost: 10500,
+    level: '고수'
 })
