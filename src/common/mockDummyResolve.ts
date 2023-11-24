1eac58b52998bc72614564b889ec9b4adc2148bf
westwoong <westwoong>
2023-11-24 16:37:17 +0900

    Test : UserEntity 생성자 변경에 따른 mock데이터 수정
    테스트용 salt 키값 추가

--- src/common/mockDummyResolve.ts
+++ src/common/mockDummyResolve.ts
@@ -7,6 +7,7 @@ import {CategoriesEntity} from "../categories/Categories.entity";
 export const DUMMY_USER_RESOLVE = new UserEntity({
     account: "xptmxmlqslek123",
     password: "testpassword123",
+    salt: "testPasswordSalt",
     name: "홍길동",
     email: "test11r@example.com",
     phoneNumber: "01052828282",
