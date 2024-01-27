e7e7043436aaf4f28d2d9f0051926bfbbdec6680
westwoong <westwoong>
2024-01-27 19:25:13 +0900

    Test : jwt key 하드코딩 테스트/워크플로우 수정

--- src/auth/jwtPassport/jwt.strategy.ts
+++ src/auth/jwtPassport/jwt.strategy.ts
@@ -13,7 +13,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET_KEY,
+            // secretOrKey: process.env.JWT_SECRET_KEY,
+            secretOrKey: 'hardCodingTest',
         });
     }
 
