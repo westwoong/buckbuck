2f6a21ecb33e052d7bf2b34b555f43e06745c5a9
westwoong <westwoong>
2024-02-22 19:34:21 +0900

    Feat : commentRepository 인터페이스 getCommetByPostIdSortedDecending() 메서드 추가

--- src/comments/comment.repository.ts
+++ src/comments/comment.repository.ts
@@ -2,6 +2,8 @@ import {CommentEntity} from "./Comment.entity";
 import {PostEntity} from "../posts/Post.entity";
 
 export interface CommentRepository {
+    getCommentByPostIdSortedDescending(postId: number, page: number): Promise<CommentEntity[] | null>
+
     findCommentWithUser(commentId: number): Promise<CommentEntity | null>
 
     findAllByPost(post: PostEntity): Promise<CommentEntity[]>
