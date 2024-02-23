af6127f7c779aed7ef0972f788b9dc2fc3ce3670
westwoong <westwoong>
2024-02-24 00:07:53 +0900

    Feat : ParamIdRequestDto 제거
    validator 유효성 검증이 필요하지 않는 한
    의미가 없는걸로 판단되어 삭제 진행

--- src/common/paramId.request.dto.ts
+++
@@ -1,8 +0,0 @@
-import {IsNotEmpty, IsString, Matches} from "class-validator";
-
-export class ParamIdRequestDto {
-    @IsNotEmpty()
-    @IsString()
-    @Matches(/^[0-9]+$/, { message: '숫자로 된 paramId를 입력해주세요' })
-    paramId: string;
-}
