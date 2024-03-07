6ad16e750970a259929b6484a9fdb6df6b3c6133
westwoong <westwoong>
2024-03-08 00:11:48 +0900

    Feat : AuthModule dotenv.config 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,14 @@ import {Module} from '@nestjs/common';
 import {AuthService} from './auth.service';
 import {JwtModule} from "@nestjs/jwt";
 import * as process from "process";
-require('dotenv').config()
+import * as path from 'path';
+
+require('dotenv').config({
+    path: path.resolve(
+        process.env.NODE_ENV === 'product' ? '.env.product' :
+            process.env.NODE_ENV === 'develop' ? '.env.develop' : '.env.local'
+    )
+});
 
 @Module({
     imports: [
