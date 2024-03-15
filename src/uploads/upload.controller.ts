be2dc370a2646fa2e1196d756ef51392203bf3c3
westwoong <westwoong>
2024-03-16 05:57:08 +0900

    Feat : /upload 컨트롤러 추가

--- src/uploads/upload.controller.ts
+++ src/uploads/upload.controller.ts
@@ -1,9 +1,19 @@
-import {Controller} from "@nestjs/common";
+import {Controller, HttpCode, Post, UploadedFiles, UseGuards, UseInterceptors} from "@nestjs/common";
 import {UploadService} from "./upload.service";
+import {JwtAuthGuard} from "../auth/jwtPassport/jwtAuth.guard";
+import {FilesInterceptor} from "@nestjs/platform-express";
 
-@Controller()
+@Controller('upload')
 export class UploadController {
     constructor(private readonly uploadService: UploadService) {
+    }
+
+    @Post('/')
+    @HttpCode(201)
+    @UseGuards(JwtAuthGuard)
+    @UseInterceptors(FilesInterceptor('files'))
+    async upload(@UploadedFiles() files: Array<Express.Multer.File>) {
+        return this.uploadService.fileUpload(files);
     }
 }
 
