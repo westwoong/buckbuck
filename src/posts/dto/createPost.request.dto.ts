07467e9d540e805e5a71305c790a4110cf4c7c17
westwoong <westwoong>
2024-03-17 02:42:20 +0900

    Feat : createPostRequestDto images 필드 추가 (+Swagger 문서 추가)
    업로드한 이미지의 주소를 넣는 필드 images
    
    옵셔널 선택 가능

--- src/posts/dto/createPost.request.dto.ts
+++ src/posts/dto/createPost.request.dto.ts
@@ -1,10 +1,10 @@
-import {IsInt, IsNotEmpty, IsString, Length, Max, Min} from "class-validator";
+import {IsArray, IsInt, IsNotEmpty, IsString, Length, Max, Min, Matches, IsOptional} from "class-validator";
 import {ApiProperty} from "@nestjs/swagger";
 
 export class CreatePostRequestDto {
     @IsNotEmpty()
     @IsString()
-    @Length(1,20)
+    @Length(1, 20)
     @ApiProperty({
         description: '1~20글자 사이의 게시글 제목',
         example: '제목입니다.',
@@ -12,9 +12,27 @@ export class CreatePostRequestDto {
     })
     title: string;
 
+    @IsArray()
+    @IsString()
+    @IsOptional()
+    @Matches(
+        /^https:\/\/buckbuck-uploaded\.s3\.ap-northeast-2\.amazonaws\.com\/.*$/,
+        {
+            each: true, // URL 검증
+            message: 'https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com 내에 있는 이미지만 허용됩니다.'
+        })
+    @ApiProperty({
+        description: '업로드한 이미지 S3 주소',
+        example: [
+            "https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com/1710605624135-images.jpeg",
+            "https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com/1710605624136-184293.jpg"
+        ]
+    })
+    images?: Array<string>
+
     @IsNotEmpty()
     @IsString()
-    @Length(1,300)
+    @Length(1, 300)
     @ApiProperty({
         description: '1~300글자 사이의 게시글 내용',
         example: '본문 내용입니다.',
@@ -35,7 +53,7 @@ export class CreatePostRequestDto {
 
     @IsNotEmpty()
     @IsString()
-    @Length(1,10)
+    @Length(1, 10)
     @ApiProperty({
         description: '1~10글자 사이의뢰 난이도 레벨',
         example: '어려울거같아요',
