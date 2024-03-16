13f0659799199b8a55272aa2f75543f308cdb82d
westwoong <westwoong>
2024-03-17 01:16:26 +0900

    Feat : POST /upload ReponseDTO 추가, url location 인터페이스 타입적용

---
+++ src/uploads/dto/upload.response.dto.ts
@@ -0,0 +1,19 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {UploadEntity} from "../upload.entity";
+
+export class UploadResponseDto {
+    @ApiProperty({
+        description: '업로드 된 파일의 S3 URL',
+        example: {
+            "location": [
+                "https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com/1710605624135-images.jpeg",
+                "https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com/1710605624136-184293.jpg"
+            ]
+        },
+    })
+    location: string[];
+
+    constructor(files: UploadEntity[]) {
+        this.location = files.map(file => (file.url));
+    }
+}
--- src/uploads/upload.service.ts
+++ src/uploads/upload.service.ts
@@ -3,6 +3,8 @@ import {UploadRepository} from "./upload.repository";
 import {UploadEntity} from "./upload.entity";
 import {UPLOAD_REPOSITORY} from "../common/injectToken.constant";
 import {Transactional} from "typeorm-transactional";
+import {UploadResponseDto} from "./dto/upload.response.dto";
+import {MulterS3FileLocation} from "./dto/upload.location.interface";
 
 @Injectable()
 export class UploadService {
@@ -18,14 +20,14 @@ export class UploadService {
             const uploadFiles: UploadEntity[] = [];
 
             for (const file of files) {
-                let url = (file as any).location;
+                let url = (file as MulterS3FileLocation).location;
                 let sequence = uploadFiles.length + 1;
                 const uploadEntity = new UploadEntity({url, sequence});
                 uploadFiles.push(uploadEntity)
             }
             await this.uploadRepository.uploadFile(uploadFiles);
 
-            return uploadFiles;
+            return new UploadResponseDto(uploadFiles);
         } catch (error) {
             throw new ServiceUnavailableException(`업로드에 실패하였습니다: ${error}`)
         }
