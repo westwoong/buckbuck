99d6723556ff2e9a9156bd0d115a582fb4ec115b
westwoong <westwoong>
2024-03-24 06:54:46 +0900

    Feat : S3 이미지 삭제 로직, 컨트롤러 추가
    DELETE /uploads/:imageId
    
    upload 기능 컨트롤러 메서드명 수정 test -> upload
    
    S3 이미지가 삭제되었을 경우에만 DB에 기록된 데이터 삭제 처리

--- src/uploads/upload.controller.ts
+++ src/uploads/upload.controller.ts
@@ -1,4 +1,14 @@
-import {Controller, HttpCode, Post, UploadedFiles, UseGuards, UseInterceptors} from "@nestjs/common";
+import {
+    Controller,
+    Delete,
+    HttpCode,
+    Param,
+    ParseIntPipe,
+    Post,
+    UploadedFiles,
+    UseGuards,
+    UseInterceptors
+} from "@nestjs/common";
 import {UploadService} from "./upload.service";
 import {JwtAuthGuard} from "../auth/jwtPassport/jwtAuth.guard";
 import {FilesInterceptor} from "@nestjs/platform-express";
@@ -7,7 +17,7 @@ import {ApiBearerAuth, ApiBody, ApiConsumes, ApiHeader, ApiOperation, ApiRespons
 import {UploadResponseDto} from "./dto/upload.response.dto";
 
 @ApiTags('파일 업로드 API')
-@Controller('upload')
+@Controller('uploads')
 export class UploadController {
     constructor(private readonly uploadService: UploadService) {
     }
@@ -37,8 +47,14 @@ export class UploadController {
     })
     @UseGuards(JwtAuthGuard)
     @UseInterceptors(FilesInterceptor('postImages', 5, multerS3Config()))
-    async test(@UploadedFiles() postImages: Array<Express.Multer.File>) {
+    async upload(@UploadedFiles() postImages: Array<Express.Multer.File>) {
         return await this.uploadService.fileUpload(postImages);
+    }
+
+    @Delete(':imageId')
+    @HttpCode(204)
+    async delete(@Param('imageId', ParseIntPipe) imageId: number) {
+        return await this.uploadService.deleteByImageId(imageId);
     }
 
 }
