cfff40e506e392f6f956d220e23f1d87ee3eca64
westwoong <westwoong>
2024-03-26 11:43:36 +0900

    Docs : Swagger UploadResponseDto 응답값 예시 수정

--- src/uploads/dto/upload.response.dto.ts
+++ src/uploads/dto/upload.response.dto.ts
@@ -6,8 +6,8 @@ export class UploadResponseDto {
         description: '업로드 된 파일의 S3 URL',
         example:
             [
-                "https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com/1710605624135-images.jpeg",
-                "https://buckbuck-uploaded.s3.ap-northeast-2.amazonaws.com/1710605624136-184293.jpg"
+                "1710605624135-images.jpeg",
+                "1710605624136-184293.jpg"
             ]
     })
     location: string[];
